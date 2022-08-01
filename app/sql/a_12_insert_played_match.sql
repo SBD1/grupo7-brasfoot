1617e895ed649ee1827e8d9d69206e6c35ac1cea
Paulo Gontijo <paulohgontijoo@gmail.com>
2022-08-01 17:27:29 -0300

    Adjusting query for insert played_match

--- app/sql/a_12_insert_played_match.sql
+++ app/sql/a_12_insert_played_match.sql
@@ -8,19 +8,19 @@ INSERT INTO played_match (
 )
 
 VALUES (
-    (SELECT id FROM public.match WHERE (match.date = '01/01/2023' & match.id_team_host = 'Cruzeiro'),
+    (SELECT match.id FROM match, team WHERE (match.date = '01/01/2023' AND match.id_team_host = team.id  AND team.name = 'Cruzeiro')),
     (SELECT id FROM public.championship WHERE championship.is_cup = FALSE),
     50000,
     1000000
 ),
 (
-    (SELECT id FROM public.match WHERE (match.date = '02/02/2024' & match.id_team_host = 'Flamengo'),
+    (SELECT match.id FROM match, team WHERE (match.date = '02/02/2024' AND match.id_team_host = team.id  AND team.name = 'Flamengo')),
     (SELECT id FROM public.championship WHERE championship.is_cup = FALSE),
     60000,
     2000000
 ),
 (
-    (SELECT id FROM public.match WHERE (match.date = '03/03/2024' & match.id_team_host = 'Corinthians'),
+    (SELECT match.id FROM match, team WHERE (match.date = '03/03/2024' AND match.id_team_host = team.id  AND team.name = 'Corinthians')),
     (SELECT id FROM public.championship WHERE championship.is_cup = FALSE),
     70000,
     3000000
