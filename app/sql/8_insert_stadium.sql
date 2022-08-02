1bf97688d47957472ddcbdf7366742fa19093d6a
Paulo Gontijo <paulohgontijoo@gmail.com>
2022-08-01 22:00:05 -0300

    Adding new inserts to stadium table > > > Co-authored-by: Matheus Clemente <matheus.klementt@gmail.com>

--- app/sql/8_insert_stadium.sql
+++ app/sql/8_insert_stadium.sql
@@ -12,12 +12,42 @@ VALUES (
 	15.00
 ),
 (
-    (SELECT id FROM public.team WHERE team.name = 'Cruzeiro'),
+    (SELECT id FROM public.team WHERE team.name = 'Palmeiras'),
 	50000,
 	10.00
 ),
 (
     (SELECT id FROM public.team WHERE team.name = 'Flamengo'),
+	45000,
+	5.00
+),
+(
+    (SELECT id FROM public.team WHERE team.name = 'Fluminense'),
+	50000,
+	10.00
+),
+(
+    (SELECT id FROM public.team WHERE team.name = 'AthleticoPR'),
+	45000,
+	5.00
+),
+(
+    (SELECT id FROM public.team WHERE team.name = 'Internacional'),
+	50000,
+	10.00
+),
+(
+    (SELECT id FROM public.team WHERE team.name = 'AtleticoMG'),
+	45000,
+	5.00
+),
+(
+    (SELECT id FROM public.team WHERE team.name = 'Bragantino'),
+	50000,
+	10.00
+),
+(
+    (SELECT id FROM public.team WHERE team.name = 'SaoPaulo'),
 	45000,
 	5.00
 );
