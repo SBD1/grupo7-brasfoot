19e3957a5358e8e71f702e70e864b302cd5570c5
Paulo Gontijo <paulohgontijoo@gmail.com>
2022-09-11 21:06:17 -0300

    Improoving finance inserts

--- app/sql/a_15_insert_finance.sql
+++ app/sql/a_15_insert_finance.sql
@@ -9,17 +9,52 @@ INSERT INTO finance (
 VALUES (
     (SELECT team.id FROM team WHERE (team.name = 'Cruzeiro')),
     ('Cruzeiro'),
-    250000000.00
+    1000000.00
 ),
 (
     (SELECT team.id FROM team WHERE (team.name = 'Corinthians')),
     ('Corinthians'),
-    500000000.00
+    1000000.00
 ),
 (
     (SELECT team.id FROM team WHERE (team.name = 'Flamengo')),
     ('Flamengo'),
-    120000000.00
+    1000000.00
+),
+(
+    (SELECT team.id FROM team WHERE (team.name = 'AthleticoPR')),
+    ('AthleticoPR'),
+    1000000.00
+),
+(
+    (SELECT team.id FROM team WHERE (team.name = 'AtleticoMG')),
+    ('AtleticoMG'),
+    1000000.00
+),
+(
+    (SELECT team.id FROM team WHERE (team.name = 'Bragantino')),
+    ('Bragantino'),
+    1000000.00
+),
+(
+    (SELECT team.id FROM team WHERE (team.name = 'Fluminense')),
+    ('Fluminense'),
+    1000000.00
+),
+(
+    (SELECT team.id FROM team WHERE (team.name = 'Internacional')),
+    ('Internacional'),
+    1000000.00
+),
+(
+    (SELECT team.id FROM team WHERE (team.name = 'Palmeiras')),
+    ('Palmeiras'),
+    1000000.00
+),
+(
+    (SELECT team.id FROM team WHERE (team.name = 'SaoPaulo')),
+    ('SaoPaulo'),
+    1000000.00
 );
 
 COMMIT TRANSACTION;
